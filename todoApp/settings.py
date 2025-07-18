9f6a62f186d253efe28411215acd4433b9e1b481
Sankalp <sankalp@Sakshi.localdomain>
2025-07-18 20:03:14 +0530

    added requirements

--- todoApp/settings.py
+++ todoApp/settings.py
@@ -84,7 +84,7 @@ DATABASES = {
 
 # Password validation
 # https://docs.djangoproject.com/en/2.2/ref/settings/#auth-password-validators
-
+DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 AUTH_PASSWORD_VALIDATORS = [
     {
         'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
